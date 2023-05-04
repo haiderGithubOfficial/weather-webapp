21493c58a0e2a1e54ddb9d4cdee5f6249b5a992f
haiderGithubOfficial <haider.official.gfx@gmail.com>
2023-05-04 15:59:31 +0500

    weather-app

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+    method: "GET",
+    headers: {
+        "X-RapidAPI-Key": "6017583aa0mshc133c9930051243p159099jsn594771a36ff7",// enter your rapid api key here
+        "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+    },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "20a513654cd7dfd2abc4f2283aeded4e"; // enter your key from openweather API
+
